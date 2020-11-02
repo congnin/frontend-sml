6a0916213da2f0a392efd3d59f4e823ba80a5304
dinhquoc689 <quocnd@scommerce.asia>
2020-11-02 15:59:25 +0700

    no message

--- complete-node-bootcamp-master/4-natours/after-section-14/public/js/stripe.js
+++ complete-node-bootcamp-master/4-natours/after-section-14/public/js/stripe.js
@@ -1,15 +1,15 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
+const stripe = Stripe('pk_test_51HixFuGskkzfD3hA2khy5cn0afJzoWbWJHiDt1oiwObdKJRrBPKsj0xF2B6HbrdzQQJ6jQrhNJfiTvNtlkFWXd4C009hyQjDJb');
 
 export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
-    // console.log(session);
+    console.log(session);
 
-    // 2) Create checkout form + chanre credit card
+    // 2) Create checkout form + change credit card
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id
     });
--- complete-node-bootcamp-master/4-natours/after-section-14/readme.md
+++ complete-node-bootcamp-master/4-natours/after-section-14/readme.md
@@ -1,3 +1,37 @@
 # Natours Application
 
 Built using modern technologies: node.js, express, mongoDB, mongoose and friends 😁
+
+## Import development data
+
+```
+node dev-data/data/import-dev-data.js --import
+```
+
+## Delete dev data
+
+```
+node dev-data/data/import-dev-data.js --delete
+```
+
+## Send Emails with Nodemailer
+
+1. Go to [mailtrap](https://mailtrap.io)
+2. Sign up with Github account
+3. Using Sendgrid for 'Real' Emails
+
+### Credit Card Payments with Stripe
+
+1. [Register](https://dashboard.stripe.com/register)
+2. cong\*2406@gmail.com - Th\*541
+3. edit template stripe
+
+![](image/stripe.png)
+
+### Data Sanitization
+
+![](image/sanitization.png)
+
+### Geospatial Queries
+
+![](image/schema_analize.png)
