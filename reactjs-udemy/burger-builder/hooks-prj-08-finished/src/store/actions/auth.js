700d187fb647320a634b589f696008114f61fdb5
dinhquoc689 <quocnd@scommerce.asia>
2020-11-12 21:51:31 +0700

    no message

--- reactjs-udemy/burger-builder/hooks-prj-08-finished/src/store/actions/auth.js
+++ reactjs-udemy/burger-builder/hooks-prj-08-finished/src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDbMS8I4RjzhXU5pDBanXiwKliwF2aQpV4';
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAvXK_TSSOH7w4nr6A-6N0wszs-MGhZtw8';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDbMS8I4RjzhXU5pDBanXiwKliwF2aQpV4';
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAvXK_TSSOH7w4nr6A-6N0wszs-MGhZtw8';
         }
         axios.post(url, authData)
             .then(response => {
