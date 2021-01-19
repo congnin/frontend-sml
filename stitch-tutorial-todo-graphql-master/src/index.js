8dfb602ed1d116e531f77ea6e43a64219dfd2cf3
dinhquoc689 <quocnd@scommerce.asia>
2021-01-19 15:18:35 +0700

    no message

--- stitch-tutorial-todo-graphql-master/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// TODO: render the application with an ApolloProvider
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
