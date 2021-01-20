697e14f46b61d4d9b64d049d86f59f1821b683e4
dinhquoc689 <quocnd@scommerce.asia>
2021-01-20 20:12:16 +0700

    soundcloud

--- react-todo-master/src/index.js
+++ react-todo-master/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from 'store'
-import App from './containers/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import App from "./containers/App";
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
-)
+  document.getElementById("root")
+);
